bd9045232b2cc49b65e1f343950dc5af8d594bb6
BSS-Lord <61976493+BSS-Lord@users.noreply.github.com>
2020-08-23 09:46:06 +0300

    Update vertex-shader.glsl

--- Engine/Shaders/vertex-shader.glsl
+++ Engine/Shaders/vertex-shader.glsl
@@ -1,14 +1,17 @@
 attribute vec2 a_position;
 attribute vec2 a_texcoord;
+attribute vec4 a_color;
 
 uniform mat3 u_matrix;
 
 varying vec2 v_texcoord;
+varying vec4 v_color;
 
 void main () {
 	
 	gl_Position = vec4((u_matrix * vec3(a_position, 1)).xy, 0, 1);
 	
 	v_texcoord = a_texcoord;
+	v_color = a_color;
 	
 }
