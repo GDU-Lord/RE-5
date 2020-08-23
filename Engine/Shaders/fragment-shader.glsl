a630581d27d227556ab8f1107d9a4a2eca84b67e
BSS-Lord <61976493+BSS-Lord@users.noreply.github.com>
2020-08-23 09:45:47 +0300

    Update fragment-shader.glsl

--- Engine/Shaders/fragment-shader.glsl
+++ Engine/Shaders/fragment-shader.glsl
@@ -5,10 +5,11 @@ uniform vec2 u_opacity;
 uniform vec4 u_filter;
 
 varying vec2 v_texcoord;
+varying vec4 v_color;
 
 void main () {
 
-	gl_FragColor = texture2D(u_texture, v_texcoord) * u_filter;
+	gl_FragColor = texture2D(u_texture, v_texcoord) * v_color * u_filter;
 	gl_FragColor.a *= u_opacity[0];
 	gl_FragColor.rgb *= gl_FragColor.a;
 	
